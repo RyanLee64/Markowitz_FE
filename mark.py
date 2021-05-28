b2c367fff05b70ec137d2bb922d2a295a8456c0a
Ryan Lee <dbl2127@columbia.edu>
2021-05-28 00:26:19 -0400

    markowitz optimization with accompanying excel sheet

---
+++ mark.py
@@ -0,0 +1,86 @@
+#%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import cvxopt as opt
+from cvxopt import blas, solvers
+import pandas as pd
+
+NUM_PORTFOLIOS = 1000000
+NUM_STOCKS = 20
+SHEET_NAME = 'returns'
+RISK_FREE_RATE = .02
+FILE_NAME = 'markowitz_assets.xlsx'
+# Turn off progress printing
+solvers.options['show_progress'] = False
+
+
+def read_returns(xl_file, sheet ):
+    df = pd.read_excel(xl_file, engine='openpyxl', sheet_name=sheet)
+    df = df.drop(columns=['Date'])
+    return df
+
+
+def rand_weights(n):
+    ''' Produces n rancdom weights that sum to 1 '''
+    k = np.random.randn(n)
+   # print(k/sum(k))
+    return k / sum(k)
+
+
+def find_optimal_risky(means, stds, weights):
+    sharp_return = 0
+    sharp_std = 0
+    sharp_rat = 0
+    sharp_weight = []
+    for x, y, z in zip(means, stds, weights):
+        if (x-RISK_FREE_RATE)/y > sharp_rat:
+            sharp_return = x
+            sharp_std = y
+            sharp_rat = (x-RISK_FREE_RATE)/y
+            sharp_weight = z
+    return sharp_return, sharp_std, sharp_weight
+
+
+def rand_portfolio_stats(data_frame):
+
+    cov = np.asmatrix((data_frame.cov()).to_numpy())
+    avg = np.asmatrix((data_frame.mean()).to_numpy())
+    weights = np.asmatrix(rand_weights(NUM_STOCKS))
+    portfolio_avg = avg * weights.T
+    sigma = np.sqrt(weights * cov * weights.T)
+    if (sigma > 5 or portfolio_avg < -1 or portfolio_avg > 1):
+        return rand_portfolio_stats(data_frame)
+    return portfolio_avg, sigma, weights
+
+
+def main():
+    df = read_returns(FILE_NAME, SHEET_NAME)
+
+    means = []
+    stds = []
+    weights = []
+    for x in range(NUM_PORTFOLIOS):
+        mean, std, weight = rand_portfolio_stats(df)
+        means.append(mean.item(0))
+        stds.append(std.item(0))
+        weights.append(weight)
+    opt_mean, opt_std, opt_weights = find_optimal_risky(means, stds, weights)
+    fig = plt.figure()
+    plt.plot(stds, means, 'o', markersize=5)
+    plt.plot(opt_std, opt_mean, 'ro')
+    plt.plot()
+    plt.xlabel('std')
+    plt.ylabel('mean')
+    plt.title('Mean and standard deviation of returns of randomly generated portfolios')
+    print(opt_weights)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
